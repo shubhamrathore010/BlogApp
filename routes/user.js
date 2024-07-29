c7cf6ce8c616da59af1550c117752342f6e25e19
Shubham <shubhamrathore2865@gmail.com>
2024-07-29 22:25:32 +0530

    Initial commit

---
+++ routes/user.js
@@ -0,0 +1,58 @@
+const { Router } = require("express");
+// const { models } = require("mongoose");
+const User = require('../models/user');
+const { render } = require("ejs");
+
+
+const router =  Router();
+
+router.get('/', (req, res) => {
+    res.render('home', {
+      user: req.user,
+    })
+  })
+  
+router.get('/signin', (req, res) => {
+    return res.render('signin');
+});
+
+router.get('/signup', (req, res) => {
+    return res.render('signup');
+});
+
+router.post('/signin', async (req, res) => {
+    const { email , password } = req.body;
+ try{
+  const token = await  User.matchPasswordAndGenerateToken(email, password)
+     
+  console.log(token);
+  return res.cookie("token", token).redirect('/')
+ }catch(error){
+    return res.render("signin",{
+        error: "Incorrect Email and Password",
+        
+    })
+ }
+})
+
+router.post('/signup', async (req, res) => {
+   
+ try {
+    const { fullName, email, password, } = req.body;
+    await User.create({
+        fullName,
+        email,
+        password,
+    });
+    return res.redirect('/')
+ } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Error creating user');
+ }
+});
+
+router.get('/logout',(req, res) => {
+    res.clearCookie('token').redirect('/')
+    console.log(token);
+})
+module.exports = router;
